6407a3f26a1c9673988f17548d55305870601e72
noxtsu <94978861+noxtsu@users.noreply.github.com>
2022-06-15 21:09:04 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const API_KEY = "c3e1b48fd1b2589a9902448ce98a2a9c";
+
+$(function(){
+  $('#btn').on('click', function(){
+    //入力された都市名でWebAPIに天気情報をリクエスト
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    }).done(function (data){
+      //通信成功
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src","http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt",data.weather[0].main);
+
+    }).fail(function (data){
+      //通信失敗
+      alert('通信に失敗しました。');
+    });
+
+  });
+});
